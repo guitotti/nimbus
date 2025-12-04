e8efdad826a2f0dffac5d80ce30deef69c1429de
Guilherme Totti <dev.guilhermetotti@gmail.com>
2025-12-03 22:08:17 -0300

    feat: create current weather response type

---
+++ web/src/api/types.ts
@@ -0,0 +1,17 @@
+export interface CurrentWeatherResponse {
+  cidade: string,
+  clima_principal: string,
+  codigo_pais: string,
+  descricao_clima: string,
+  direcao_vento_graus: number,
+  id_cidade: number,
+  latitude: number,
+  longitude: number,
+  pressao_hpa: number,
+  sensacao_termica_celsius: number,
+  temperatura_atual_celsius: number,
+  temperatura_max_celsius: number,
+  temperatura_min_celsius: number,
+  umidade_porcentagem: number,
+  velocidade_vento_m_s: number,
+}
